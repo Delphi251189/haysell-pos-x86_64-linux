cc4f7483e1dfa8d3651f47df0a9d4f1bf063756e
Delphi251189 <delphi251189@gmail.com>
2023-12-28 00:40:23 +0400

    install script improovments

--- init.sh
+++ init.sh
@@ -3,24 +3,10 @@
 mkdir -p /usr/share/applications
 cp ./desktop/haysell-pos.desktop /usr/share/applications/haysell-pos.desktop
 
-mkdir -p /usr/share/icons/hicolor/16x16/apps
-mkdir -p /usr/share/icons/hicolor/20x20/apps
-mkdir -p /usr/share/icons/hicolor/22x22/apps
-mkdir -p /usr/share/icons/hicolor/24x24/apps
-mkdir -p /usr/share/icons/hicolor/32x32/apps
-mkdir -p /usr/share/icons/hicolor/36x36/apps
-mkdir -p /usr/share/icons/hicolor/48x48/apps
-mkdir -p /usr/share/icons/hicolor/64x64/apps
-mkdir -p /usr/share/icons/hicolor/128x128/apps
-mkdir -p /usr/share/icons/hicolor/256x256/apps
+for VARIABLE in 16 20 22 24 32 36 48 64 128 256
+do
+	mkdir -p /usr/share/icons/hicolor/${VARIABLE}x${VARIABLE}/apps
+	cp ./icons/${VARIABLE}x${VARIABLE}.png /usr/share/icons/hicolor/${VARIABLE}x${VARIABLE}/apps/haysell.png
+done
 
-cp ./icons/16x16.png /usr/share/icons/hicolor/16x16/apps/haysell.png
-cp ./icons/20x20.png /usr/share/icons/hicolor/20x20/apps/haysell.png
-cp ./icons/22x22.png /usr/share/icons/hicolor/22x22/apps/haysell.png
-cp ./icons/24x24.png /usr/share/icons/hicolor/24x24/apps/haysell.png
-cp ./icons/32x32.png /usr/share/icons/hicolor/32x32/apps/haysell.png
-cp ./icons/36x36.png /usr/share/icons/hicolor/36x36/apps/haysell.png
-cp ./icons/48x48.png /usr/share/icons/hicolor/48x48/apps/haysell.png
-cp ./icons/64x64.png /usr/share/icons/hicolor/64x64/apps/haysell.png
-cp ./icons/128x128.png /usr/share/icons/hicolor/128x128/apps/haysell.png
-cp ./icons/256x256.png /usr/share/icons/hicolor/256x256/apps/haysell.png
+ln -s `pwd`/haysell-pos /usr/bin/haysell-pos
